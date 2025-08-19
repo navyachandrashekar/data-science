b80e82f0daf51cbfef23c796b37cf11cf430dd52
navyachandrashekar <navyachandrashekar2210@gmail.com>
2025-08-19 11:31:28 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,24 @@
+list=[1,1,2,2,3,3,4,4,5,5]
+print(set(list))
+
+
+
+
+a=64**0.5
+print(a)
+
+s ='hvhbnjbhskjbbhdnjssbhsbwhsjwjnsbjello'
+print(s.index('h'))
+  
+s=[10,20,40,20,90]
+print(s[1:4:2])
+
+
+a=[0]
+print(a*3)
+
+b=[1-1]
+print(b*3)
+
+c=[1,2,[3,4,'hello']]
+print
